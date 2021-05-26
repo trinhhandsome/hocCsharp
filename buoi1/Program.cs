e3a4a0f177372f1e21ed682cd23e35bdff1e5799
Your Name <you@example.com>
2021-05-26 21:37:42 +0700

    first commit

---
+++ buoi1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace buoi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
